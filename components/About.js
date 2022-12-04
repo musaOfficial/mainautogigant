918d729bd88d80a1502566337a2bb2f973649be9
Musa Arslan <officialmusa58@gmail.com>
2022-12-04 14:07:20 +0100

    Update

--- components/About.js
+++ components/About.js
@@ -11,17 +11,28 @@ function About(){
                 <div className={classes.companydescriptioncontainer}>
                     <div className={classes.lineleft}></div>
                     <div className={classes.companydesc}>
-                        <h1>ÜBER UNS</h1>
-                        <h3>Autogigant.at</h3>
-                        <p>Der führende Online-Marktplatz auf dem österreichischen Automarkt, wurde 2022 gegründet und gehört zur GIGANT GROUP.</p>
-                        <h3>Mit der Vision.</h3>
-                        <p>Die führende, innovativste und zuverlässigste Technologieplattform des Automobilhandels und -Services zu sein, konzentriert sich das Team von Autogigant.at 
+                        <h1 className={classes.desc1}>ÜBER UNS</h1>
+                        <h3 className={classes.desc2}>Autogigant.at</h3>
+                        <p className={classes.desc3}>Der führende Online-Marktplatz auf dem österreichischen Automarkt, wurde 2022 gegründet und gehört zur GIGANT GROUP.</p>
+                        <h3 className={classes.desc4}>Mit der Vision.</h3>
+                        <p className={classes.desc5}>Die führende, innovativste und zuverlässigste Technologieplattform des Automobilhandels und -Services zu sein, konzentriert sich das Team von Autogigant.at 
 auf die Perfektionierung des gesamten Systems und ist bestrebt, die Erfahrung seiner Nutzer mit neuen Funktionen jeden Monat zu verbessern.</p>
-                        <p>Autogigant.at ist nicht nur eine Plattform für Inserate, 
+                        <p className={classes.desc6}>Autogigant.at ist nicht nur eine Plattform für Inserate, 
 sondern bietet seinen Nutzern mit dem Sofortkaufsystem Trink sell!, dem Marktwertermittlungstool Arabam Kaç Para, 
 der Funktion arabam/garage, bei der Sie sowohl die sofortige Marktwertentwicklung verfolgen als auch alles rund um Ihr Fahrzeug zu Sonderpreisen 
 kaufen können, und dem arabam/supply-Service für die Fahrzeugbeschaffung seiner Firmenmitglieder einen Rundum-Service.</p>
-                        
+                    </div>
+                </div>
+                <div className={classes.middlecontainer}>
+                    <div className={classes.left}>
+                        <h1 className={classes.leftheading} >Unser CEO sagt</h1>
+                        <p className={classes.lefttext} >Qualität ist nie ein Zufall; 
+sie ist immer das Ergebnis 
+einer hohen Absicht.</p>
+                        <button className={classes.contactbtn}>Kontaktieren Sie uns</button>
+                    </div>
+                    <div className={classes.right}>
+                        <Image src={'/aboutus/abr.png'} layout='fill' /> 
                     </div>
                 </div>
             </div>
